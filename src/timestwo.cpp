6f888356dd982a191985469ed4c5aa6341ed5ee9
gmweaver <gmweaver@usc.edu>
2017-12-06 17:04:17 -0800

    edit example c++

--- src/timestwo.cpp
+++ src/timestwo.cpp
@@ -17,11 +17,3 @@ NumericVector timesTwo(NumericVector x) {
 }
 
 
-// You can include R code blocks in C++ files processed with sourceCpp
-// (useful for testing and development). The R code will be automatically
-// run after the compilation.
-//
-
-/*** R
-timesTwo(42)
-*/
