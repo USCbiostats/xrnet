dd6c5492f0a7cff262a82f6742956bdd292bb744
gmweaver <gmweaver@usc.edu>
2018-01-18 17:06:31 -0800

    add example data

---
+++ R/data.R
@@ -0,0 +1,14 @@
+#' Simulated predictor data
+#'
+#' @format A matrix with 100 rows and 50 variables
+"x"
+
+#' Simulated outcome data
+#'
+#' @format A vector with 100 elements
+"y"
+
+#' Simulated external data
+#'
+#' @format A matrix with 50 rows and 4 columns
+"ext"
