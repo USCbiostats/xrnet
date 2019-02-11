6db35278a7654016ca0aed22fedd16f0c2e0c8da
gmweaver <gmweaver@usc.edu>
2019-02-11 15:54:53 -0800

    get number in strong/active sets

--- src/rcpp_wrappers_fit.cpp
+++ src/rcpp_wrappers_fit.cpp
@@ -169,6 +169,8 @@ Rcpp::List fitModel(const TX & x,
             Rcpp::Named("penalty") = ys * path,
             Rcpp::Named("penalty_ext") = ys * path_ext,
             Rcpp::Named("num_passes") = solver->getNumPasses(),
+            Rcpp::Named("strong_sum") = strong_sum,
+            Rcpp::Named("active_sum") = active_sum,
             Rcpp::Named("family") = family,
             Rcpp::Named("status") = status
         );
