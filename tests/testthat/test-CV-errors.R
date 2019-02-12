63946b3c86d017012499f9f4b7bc5901f508b24c
gmweaver <gmweaver@usc.edu>
2019-02-12 00:13:07 -0800

    update CV error test

--- tests/testthat/test-CV-errors.R
+++ tests/testthat/test-CV-errors.R
@@ -9,7 +9,7 @@ test_that("gaussian", {
 
     expect_equal(
         cv_mean,
-        cv_hierr(x = xtest,
+        check <- cv_hierr(x = xtest,
                  y = ytest,
                  external = ztest,
                  family = "gaussian",
@@ -21,4 +21,34 @@ test_that("gaussian", {
         check.attribute = FALSE
     )
 })
+
+#myPenalty <- define_penalty(penalty_type = 0,
+#                            penalty_type_ext = 1,
+#                            user_penalty = check$fitted_model$penalty,
+#                            user_penalty_ext = check$fitted_model$penalty_ext)
+
+#errormat <- matrix(NA, nrow = 400, ncol = 5)
+#for (k in 1:5) {
+#
+#    fit_fold <- hierr(x = xtest[foldid != k,],
+#                      y = ytest[foldid != k],
+#                      external = ztest,
+#                      family = "gaussian",
+#                      penalty = myPenalty,
+#                      control = list(tolerance = 1e-10))
+#
+#    betas <- rbind(as.vector(t(fit_fold$beta0)),
+#                   `dim<-`(aperm(fit_fold$betas, c(1, 3, 2)),
+#                           c(dim(fit_fold$betas)[1],
+#                             dim(fit_fold$betas)[2] * dim(fit_fold$betas)[3])))
+#
+#    predy <- cbind(1, xtest[foldid == k, ]) %*% betas
+#
+#    errormat[, k] <- apply(predy, 2, function(p) mean((ytest[foldid == k] - p)^2))
+#}
+#cv_mean <- rowMeans(errormat)
+#cv_mean <- matrix(cv_mean, nrow = 20, byrow = TRUE)
+
+
+
 
